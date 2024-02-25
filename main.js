8e6edd7ca6209a8ec0682cf68b224179e68024dc
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-02-25 12:44:54 +0300

    add operation & click handler

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 class Calculator {
   constructor(currentOperandTextElement) {
     this.currentOperandTextElement = currentOperandTextElement;
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.clear();
+  }
+  clear() {
     this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {
+    console.log("delete");
+  }
+  chooseOperand(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand + this.operation;
+    this.currentOperand = "";
+  }
+  compute() {
+    console.log("compute");
   }
   appendNumber(value) {
     // if Current Operand had (.) Dont Add It Again
@@ -11,18 +32,45 @@ class Calculator {
   // Update Display Every Time That Buttons Clicked
   updateDisplay() {
     this.currentOperandTextElement.textContent = this.currentOperand;
+    this.previousOperandTextElement.textContent = this.previousOperand;
   }
 }
 // Elements
 const buttons = document.querySelectorAll(".btn");
 const currentOperandTextElement = document.querySelector(".current-operand");
+const previousOperandTextElement = document.querySelector(".previous-operand");
 // Events
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const value = e.currentTarget.dataset.btn;
-    calculator.appendNumber(value);
-    calculator.updateDisplay();
+    clickHandler(value);
   });
 });
+const clickHandler = (value) => {
+  switch (value) {
+    case "/":
+    case "*":
+    case "-":
+    case "+":
+      calculator.chooseOperand(value);
+      break;
+    case "c":
+      calculator.clear();
+      break;
+    case "de":
+      calculator.delete();
+      break;
+    case "=":
+      calculator.compute();
+      break;
+    default:
+      calculator.appendNumber(value);
+      break;
+  }
+  calculator.updateDisplay();
+};
 // Call Calculator Class
-const calculator = new Calculator(currentOperandTextElement);
+const calculator = new Calculator(
+  currentOperandTextElement,
+  previousOperandTextElement
+);
